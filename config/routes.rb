5ccd4b0d7fa93df25a6d3863be51b185cced7a90
Hesham Salah <hs414h@gmail.com>
2021-03-08 20:57:43 +0200

    Scaffold Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210308185604_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :mail
+      t.boolean :send_due_date_reminder
+      t.integer :due_date_reminder_interval
+      t.time :due_date_reminder_time
+      t.string :time_zone
+
+      t.timestamps
+    end
+    add_index :users, :mail
+  end
+end
