69e6564b7d8d622427634f9e50bf220200380a71
Hesham Salah <hs414h@gmail.com>
2021-03-11 07:47:45 +0200

    Added some comments on the reminder model explaining why it was created

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# This model was added as an approach to keep track of the reminders the user
+# creates and operate on them, like giving the user the ability to control 
+# reminders by adding the ability to delete them, which will require finding 
+# the created job and delete it.
 class Reminder < ApplicationRecord
   belongs_to :ticket
 
